ddd7d4f04385d8904f06da9bfa368a050fbac1f0
DineshkumarUNIQ <dinesh.uniq45@outlook.com>
2026-01-03 16:24:41 +0530

    add order controler

---
+++ DNFSD_34/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DNFSD_34.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
